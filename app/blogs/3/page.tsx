3460f1100c0b33a4094271e3be013032a115eec0
alejandroyunes@outlook.com <alejandro.cano@imagineapps.co>
2024-03-19 00:26:56 -0500

    removing dynamic

---
+++ app/blogs/3/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+import * as stylex from "@stylexjs/stylex";
+import { colors, spacing, text } from "../../../app/globalTokens.stylex";
+import { blogsData } from "../../blog/blogs";
+import React from "react";
+import Image from "next/image";
+
+interface ParamsProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Blogs() {
+  const selectedBlog = blogsData[2];
+  return (
+    <main {...stylex.props(s.container)}>
+      {blogsData && (
+        <React.Fragment>
+          <h1 {...stylex.props(s.title)}>{selectedBlog.title}</h1>
+          <Image
+            src={selectedBlog.image}
+            alt=""
+            {...stylex.props(s.image)}
+            width={600}
+            height={400}
+          />
+
+          <p>{selectedBlog.blogSection1}</p>
+          <br />
+          <p>{selectedBlog.blogSection2}</p>
+          <br />
+          <p>{selectedBlog.blogSection3}</p>
+          <br />
+          <p>{selectedBlog.blogSection4}</p>
+          <br />
+        </React.Fragment>
+      )}
+    </main>
+  );
+}
+const s = stylex.create({
+  container: {
+    display: "flex",
+    marginTop: {
+      default: spacing.xxl,
+      "@media (max-width: 756px)": spacing.sm,
+    },
+    flexDirection: "column",
+  },
+  image: {
+    margin: "0 auto",
+    maxWidth: {
+      default: 600,
+      "@media (max-width: 756px)": "100%",
+    },
+    height: "auto",
+    paddingBottom: 40,
+  },
+  title: {
+    padding: {
+      default: `${spacing.sm} ${spacing.sm} ${spacing.xl}`,
+      "@media (max-width: 900px)": `${spacing.sm} ${spacing.sm} ${spacing.xl}`,
+    },
+    fontSize: text.h4,
+    position: "relative",
+    color: colors.inverted,
+    margin: "0 auto",
+  },
+  bg: {
+    position: "absolute",
+    top: -106,
+    left: -16,
+    height: 90,
+    width: 90,
+    backgroundColor: colors.black,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    lineHeight: 1,
+    color: colors.white,
+  },
+  day: {
+    fontSize: text.h1,
+  },
+  blogSlogan: {
+    fontWeight: "bold",
+    paddingBottom: spacing.xxxs,
+    fontSize: text.p,
+    color: colors.gray,
+    textDecoration: "underline",
+  },
+  button: {
+    margin: `0 auto ${spacing.xxl}`,
+    textAlign: "center",
+  },
+  left: { cursor: "pointer" },
+  center: { cursor: "pointer" },
+  right: { cursor: "pointer" },
+});
--- components/widgets/widgetBlogs/index.tsx
+++ components/widgets/widgetBlogs/index.tsx
@@ -1,24 +1,26 @@
-'use client'
-import * as stylex from "@stylexjs/stylex"
-import { colors, spacing, text } from "../../../app/globalTokens.stylex"
-import { blogsData } from "@/app/blog/blogs"
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import * as stylex from "@stylexjs/stylex";
+import { colors, spacing, text } from "../../../app/globalTokens.stylex";
+import { blogsData } from "@/app/blog/blogs";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function WidgetBlogs() {
   const handleClick = () => {
-    console.log("click")
-  }
+    console.log("click");
+  };
   return (
     <section>
       <div {...stylex.props(s.container)}>
-        {blogsData.map(blog =>
-          <Link href={`/blog/${blog.id}`} key={blog.id}>
+        {blogsData.map((blog) => (
+          <Link href={`/blogs/${blog.id}`} key={blog.id}>
             <div {...stylex.props(s.left)}>
-              <img
+              <Image
                 {...stylex.props(s.image)}
                 src={blog.image}
                 alt="alt"
+                width={300}
+                height={200}
               />
               <div {...stylex.props(s.description)}>
                 <p {...stylex.props(s.blogSlogan)}>Business</p>
@@ -28,72 +30,67 @@ export default function WidgetBlogs() {
                   <p>{blog.month}</p>
                 </div>
               </div>
-
             </div>
           </Link>
-        )}
-
-
+        ))}
       </div>
     </section>
-  )
+  );
 }
 
 const s = stylex.create({
   container: {
-    display: 'grid',
+    display: "grid",
     gap: spacing.lg,
     gridTemplateColumns: {
-      default: 'repeat(3, minmax(0, 1fr))',
-      '@media (max-width: 900px) and (min-width: 756px)': 'repeat(2, 1fr)',
-      "@media (max-width: 756px)": "1fr"
+      default: "repeat(3, minmax(0, 1fr))",
+      "@media (max-width: 900px) and (min-width: 756px)": "repeat(2, 1fr)",
+      "@media (max-width: 756px)": "1fr",
     },
     marginTop: spacing.xxl,
   },
   image: {
     maxWidth: "100%",
-    height: 'auto',
+    height: "auto",
   },
   description: {
     padding: {
       default: `${spacing.sm} ${spacing.sm} ${spacing.xl}`,
-      '@media (max-width: 900px)': `${spacing.sm} ${spacing.sm} ${spacing.xl}`
+      "@media (max-width: 900px)": `${spacing.sm} ${spacing.sm} ${spacing.xl}`,
     },
     fontSize: text.h4,
-    position: 'relative',
-    color: colors.inverted
-
+    position: "relative",
+    color: colors.inverted,
   },
   bg: {
-    position: 'absolute',
+    position: "absolute",
     top: -106,
     left: -16,
     height: 90,
     width: 90,
     backgroundColor: colors.black,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
     lineHeight: 1,
-    color: colors.white
+    color: colors.white,
   },
   day: {
-    fontSize: text.h1
+    fontSize: text.h1,
   },
   blogSlogan: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingBottom: spacing.xxxs,
     fontSize: text.p,
     color: colors.gray,
-    textDecoration: 'underline',
-
+    textDecoration: "underline",
   },
   button: {
     margin: `0 auto ${spacing.xxl}`,
-    textAlign: 'center',
+    textAlign: "center",
   },
-  left: { cursor: 'pointer' },
-  center: { cursor: 'pointer' },
-  right: { cursor: 'pointer' },
-})
+  left: { cursor: "pointer" },
+  center: { cursor: "pointer" },
+  right: { cursor: "pointer" },
+});
